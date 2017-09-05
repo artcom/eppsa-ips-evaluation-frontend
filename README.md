ed3a6de45285837432a2d63cbd9d5d439c3cae85
Stefan Dieterle <stefand@artcom.de>
2017-09-05 10:23:40 +0200

    Create basic set up

---
+++ README.md
@@ -0,0 +1,3 @@
+# Indoor positioning systems evaluation frontend
+
+Frontend for setting up and running indoor positioning systems evaluation experiments.
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/* eslint-disable import/no-commonjs */
+
+const path = require("path")
+const webpack = require("webpack")
+
+const publicPath = path.join(__dirname, "public")
+
+module.exports = [
+  {
+    target: "web",
+    entry: {
+      setUp: "./src/setUp/main.js"
+    }
+  }
+].map(({ target, entry }) => {
+  const es2015 = ["es2015", { modules: false }]
+
+  return {
+    target,
+    entry,
+    output: {
+      path: publicPath,
+      filename: "[name].js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [path.resolve(__dirname, "src")],
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: target === "web" ? [es2015] : [],
+              plugins: [
+                "transform-object-rest-spread",
+                "transform-react-jsx"
+              ]
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.EnvironmentPlugin(["NODE_ENV"]),
+      new webpack.IgnorePlugin(/^(bufferutil|utf-8-validate)$/, /ws\/lib$/),
+      new webpack.NormalModuleReplacementPlugin(/any-promise\/register\.js$/, "./register-shim.js")
+    ],
+    devServer: {
+      contentBase: publicPath,
+      disableHostCheck: true,
+      host: "0.0.0.0",
+    }
+  }
+})
