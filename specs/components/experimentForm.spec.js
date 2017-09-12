792854fa8a4eaddf20544fbab734fc0d2a500999
Stefan Dieterle <stefand@artcom.de>
2017-09-12 09:11:00 +0200

    Refactor

--- specs/components/experimentForm.spec.js
+++ specs/components/experimentForm.spec.js
@@ -1,48 +1,35 @@
 /* eslint-disable  import/no-commonjs */
 import React from "react"
 import { describe, it, beforeEach, afterEach } from "mocha"
-import { expect } from "chai"
 import { shallow, mount } from "enzyme"
 import proxyquire from "proxyquire"
 import sinon from "sinon"
 import { backend } from "../../src/constants"
 import ExperimentForm from "../../src/setUp/components/experimentForm"
-import inputData from "../helpers/inputData"
-import Input, { InputField, InputLabel } from "../../src/setUp/components/input"
+import {
+  callSubmitFunctions,
+  hasInputs,
+  hasSubmit,
+  isAForm,
+  storeInput
+} from "../helpers/formHelpers"
 const experimentActions = require("../../src/setUp/actions/experimentsActions")
 
 
 describe("ExperimentForm component", () => {
   it("is a form", () => {
-    expect(shallow(<ExperimentForm />)
-      .type().displayName).to.equal("styled.form")
+    isAForm(shallow(<ExperimentForm />))
   })
 
   describe("contains", () => {
     it("a Create submit input", () => {
-      const experimentForm = shallow(<ExperimentForm />)
-      expect(experimentForm.find(InputField)).to.have.length(1)
-      expect(experimentForm.find(InputField).props().type)
-        .to.equal("submit")
-      expect(experimentForm.find(InputField).props().value)
-        .to.equal("Create")
+      hasSubmit(shallow(<ExperimentForm />), "Create")
     })
 
-    it("an Input component", () => {
-      expect(shallow(<ExperimentForm />).find(Input)).to.have.length(1)
-    })
-
-    it("an input text field for experiment name with a label", () => {
-      const experimentForm = mount(<ExperimentForm />)
-      expect(experimentForm.find(Input).childAt(0).type().displayName)
-        .to.equal("styled.label")
-      expect(experimentForm.find(Input).childAt(0).text()).to.equal("name")
-      expect(experimentForm.find(InputLabel).childAt(0).type().displayName)
-        .to.equal("styled.input")
-      expect(experimentForm.find(InputLabel).childAt(0).props().type)
-        .to.equal("text")
-      expect(experimentForm.find(InputLabel).childAt(0).props().value)
-        .to.equal("")
+    it("the expected Input components", () => {
+      hasInputs(mount(<ExperimentForm />), [
+        { name: "name", type: "text" }
+      ])
     })
   })
 
@@ -61,26 +48,25 @@ describe("ExperimentForm component", () => {
     })
 
     it("store experiment name in state when changed in input field", () => {
-      const experimentForm = mount(<ExperimentForm />)
-      inputData(experimentForm, { name: "new-experiment" })
-      expect(experimentForm.state("name")).to.equal("new-experiment")
+      storeInput(mount(<ExperimentForm />), [
+        { name: "name", value: "new-experiment" }
+      ])
     })
 
     it("call setExperiment and onSubmitted with the expected name when form is submitted",
       done => {
         const onSubmitted = sinon.spy()
         const experimentForm = mount(<ExperimentForm onSubmitted={ onSubmitted } />)
-        experimentForm.setState({ name: "new-experiment" })
-        experimentForm.simulate("submit")
-        sinon.assert.calledOnce(global.setMockExperiment)
-        sinon.assert.calledWith(
+        const input = { name: "new-experiment" }
+        const callArgs = { backend, experimentName: "new-experiment" }
+        callSubmitFunctions(
+          experimentForm,
+          input,
+          callArgs,
           global.setMockExperiment,
-          { backend, experimentName: "new-experiment" }
+          onSubmitted,
+          done
         )
-        setImmediate(() => {
-          sinon.assert.calledOnce(onSubmitted)
-          done()
-        })
       })
   })
 })
