cd0f7ce4e0fff2a4d1e07f61cfb6247033703e1a
Stefan Dieterle <16118065+globz-eu@users.noreply.github.com>
2017-10-13 16:00:54 +0200

    Log out used frontend

--- src/setUp/main.js
+++ src/setUp/main.js
@@ -6,4 +6,5 @@ import App from "./containers/app"
 
 const element = document.getElementById("app")
 console.log(`Using backend: ${config.backend}`)
+console.log(`Using frontend: ${config.frontend}`)
 render(<App backend={ config.backend } frontend={ config.frontend } />, element)
